2c58b35db646bcda38026e6d03931f55cad30bb8
kic852 <kich852@gmail.com>
2025-04-06 02:26:17 +0800

    Add files via upload

---
+++ grid.h
@@ -0,0 +1,16 @@
+// Name: Hon Ki Ching
+// Student ID: 1155213287
+// Email: 1155213287@link.cuhk.edu.hk
+
+#ifndef GRID_H
+#define GRID_H
+
+// Function prototypes
+void printGrid(int grid);
+bool isFilled(int grid, int pos);
+bool updateGrid(int &grid, int pos, char mark);
+
+/* You may put in extra function prototypes here if you have written extra
+   functions in grid.cpp. */
+
+#endif // GRID_H
