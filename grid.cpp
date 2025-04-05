f91abee7fc7c40a48aa0643a830234c0ebfc1bfc
kic852 <kich852@gmail.com>
2025-04-06 02:30:08 +0800

    Add files via upload

---
+++ grid.cpp
@@ -0,0 +1,121 @@
+// Name: Hon Ki Ching
+// Student ID: 1155213287
+// Email: 1155213287@link.cuhk.edu.hk
+
+#include "grid.h"
+#include <iostream>
+using namespace std;
+
+/* Prints grid to the screen. Do NOT modify! */
+void printGrid(int grid) {
+   char p1, p2, p3, p4, p5, p6, p7, p8, p9;   // Positions 1-9
+   int d;
+
+   d = grid / 100000000;
+   p1 = (d == 1 ? 'X' : (d == 2 ? 'O' : ' '));
+   d = grid / 10000000 % 10;
+   p2 = (d == 1 ? 'X' : (d == 2 ? 'O' : ' '));
+   d = grid / 1000000 % 10;
+   p3 = (d == 1 ? 'X' : (d == 2 ? 'O' : ' '));
+   d = grid / 100000 % 10;
+   p4 = (d == 1 ? 'X' : (d == 2 ? 'O' : ' '));
+   d = grid / 10000 % 10;
+   p5 = (d == 1 ? 'X' : (d == 2 ? 'O' : ' '));
+   d = grid / 1000 % 10;
+   p6 = (d == 1 ? 'X' : (d == 2 ? 'O' : ' '));
+   d = grid / 100 % 10;
+   p7 = (d == 1 ? 'X' : (d == 2 ? 'O' : ' '));
+   d = grid / 10 % 10;
+   p8 = (d == 1 ? 'X' : (d == 2 ? 'O' : ' '));
+   d = grid % 10;
+   p9 = (d == 1 ? 'X' : (d == 2 ? 'O' : ' '));
+
+   cout << " " << p1 << " | " << p2 << " | " << p3 << endl;
+   cout << "---+---+---" << endl;
+   cout << " " << p4 << " | " << p5 << " | " << p6 << endl;
+   cout << "---+---+---" << endl;
+   cout << " " << p7 << " | " << p8 << " | " << p9 << endl;
+}
+
+/* Returns whether position pos of grid is already filled with a mark */
+bool isFilled(int grid, int pos) {
+    // TODO; add a return statement in order to compile
+   
+   //find the pos th digit
+   for (int i=0; i < 9-pos;i++){
+      grid/=10;
+   }
+   grid%=10;
+
+   //return statement return 1 if it's empty, while 0 if it's filled
+   if (grid!=0){
+      return 1;
+   }
+   else {
+      return 0;
+   }
+}
+
+/* Models the game move from a player putting a mark in position pos of grid.
+   Returns true if a horizontal/vertical/diagonal line of three same marks is
+   formed. */
+bool updateGrid(int &grid, int pos, char mark) {
+    // TODO; add a return statement in order to compile
+
+   //update the grid digit
+   int x = (mark == 'X' ? 1 : 2 );
+
+   for (int i=0; i<9-pos;i++){
+      x *=10;
+   }
+   grid += x;
+
+   int p1, p2, p3, p4, p5, p6, p7, p8, p9; 
+   p1 = grid / 100000000;
+   p2 = grid / 10000000 % 10;
+   p3 = grid / 1000000 % 10;
+   p4 = grid / 100000 % 10;
+   p5 = grid / 10000 % 10;
+   p6 = grid / 1000 % 10;
+   p7 = grid / 100 % 10;
+   p8 = grid / 10 % 10;
+   p9 = grid % 10;
+
+   //check if there fromed same horizontally
+   if (p1 !=0 && p1 == p2 && p1 == p3){
+      return 1;
+   }
+   
+   else if (p4 !=0 && p4 == p5 && p4 == p6 ){
+      return 1;
+   }
+   else if (p7 !=0 && p7 == p8 && p7 == p9 ){
+      return 1;
+   }
+
+   //check if there formed vertically
+   else if (p1 !=0 && p1 == p4 && p1 == p7 ){
+      return 1;
+   }
+   else if (p2 !=0 && p2 == p5 && p2 == p8 ){
+      return 1;
+   }
+   else if (p3 !=0 && p3 == p6 && p3 == p9 ){
+      return 1;
+   }
+
+   //check if there formed dia
+   else if (p1 !=0 && p1 == p5 && p1 == p9 ){
+      return 1;
+   }
+   else if (p3 !=0 && p3 == p5 && p3 == p7 ){
+      return 1;
+   }
+   
+   else{
+      return 0;
+   }
+}
+
+/* You may write a main() in this file for debugging purpose, but it must be
+   removed or commented in your submitted version. */
---
+++ wildttt.cpp
@@ -0,0 +1,53 @@
+// Name: Hon Ki Ching
+// Student ID: 1155213287
+// Email: 1155213287@link.cuhk.edu.hk
+
+#include "grid.h"
+#include <iostream>
+using namespace std;
+
+
+/* Implements the game flow */
+int main() {
+    // TODO
+    int GameStatus =0; //0: still playing, 1: player1 win, 2: player2 win, 3: draw
+    int grid = 000000000; // intialise the grid
+    int player = 1,move =0,pos;
+    char mark;
+    while (GameStatus ==0){
+        
+        printGrid(grid);
+
+        
+        bool InputCon = (mark == 'X' || mark == 'O' )&&(pos>=1 && pos <=9)&& !isFilled(grid,pos);//condition expression
+        
+        //checl if the input is valid
+        do
+        {
+            cout << "Player " << player<<"'s move: ";
+            cin >> mark >> pos;
+            InputCon = (mark == 'X' || mark == 'O' )&&(pos>=1 && pos <=9)&& !isFilled(grid,pos);//condition expression
+            if (!InputCon){
+                cout << "Invalid! Try again." << endl;
+            }
+            } while (!InputCon);
+        move+=1;
+        if(updateGrid(grid,pos,mark)){
+            GameStatus = player;
+        }
+        else if(move == 9){
+            GameStatus = 3;
+        }
+        player = (player == 1 ? 2 : 1);
+        
+
+    }
+    printGrid(grid);
+    if (GameStatus == 3){
+        cout << "Draw game!"<<endl;
+    }
+    else{
+        cout << "player " << GameStatus <<" wins!";
+    }
+    return 0;
+}
